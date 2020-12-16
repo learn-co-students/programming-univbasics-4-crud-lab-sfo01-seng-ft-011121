134add7b6b3602c5709a77e85b0b19f855db0330
Erik Huang <erikhuangdrums@gmail.com>
2020-12-16 22:48:43 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,42 +1,44 @@
 def create_an_empty_array
-  my_empty_soul = []
+  []
 end
 
 def create_an_array
-  arr = [0, 0, 0, 0]
+  [4, 16, 3, 9]
 end
 
-def add_element_to_end_of_array(array, element)
-  array.push(element)
-  return array
+def add_element_to_end_of_array(arr, ele)
+  arr << ele
+  arr
 end
 
-def add_element_to_start_of_array(array, element)
-  array.unshift(element)
-  return array
+def add_element_to_start_of_array(arr, ele)
+  arr.unshift(ele)
+  arr
 end
 
-def remove_element_from_end_of_array(array)
-  return array.pop
+def remove_element_from_end_of_array(arr)
+  arr.pop
 end
 
-def remove_element_from_start_of_array(array)
-  return array.shift
+def remove_element_from_start_of_array(arr)
+  arr.shift
 end
 
-def retrieve_element_from_index(array, index_number)
-  return array[index_number]
+def retrieve_element_from_index(arr, idx)
+  arr[idx]
 end
-
+  
 def retrieve_first_element_from_array(arr)
-  return arr[0]
+  arr[0]
 end
 
 def retrieve_last_element_from_array(arr)
-  return arr[-1]
+  arr[-1]
 end
 
-def update_element_from_index(array, index_number, element)
-  array[index_number] = element
-  return element
+def update_element_from_index(arr, idx, val)
+  arr[idx] = val
+  arr[idx]
 end
+
+  
